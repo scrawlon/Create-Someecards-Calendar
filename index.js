f793a77c35e4851dc6efa52bd334f55371e87677
scrawlon <scrawlon@gmail.com>
2020-06-28 15:52:20 -0400

    Refactored calendar code into getCalendarObject function

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ try {
     localSomeecardsLinksJson = require('./ecard-links.json');
     console.log(`Loading local ecard-links.json`);
 } catch {
+    localSomeecardsLinksJson = getSomeecardLinksJson(someecardsLinksUrl)
     console.log(`Loading remote json file ${someecardsLinksUrl}`);
 }
 
@@ -36,19 +37,75 @@ async function getSomeecardLinksJson(url) {
     return await someecardsLinks.json(); 
 }
 
-async function ignoreVisualElements(page) {
-    await page.setRequestInterception(true);
-    page.on('request', (req) => {
-		const visualElements = ['image', 'stylesheet', 'font'];
+async function getCalendarObject(calendarYear) {
+    const someecardsLinks = await fetch(someecardsLinksUrl);
+    const someecardsLinksJson = Object.keys(localSomeecardsLinksJson).length
+        ? localSomeecardsLinksJson 
+        : await someecardsLinks.json(); 
+    const { baseUrl, categories, eventsByMonth } = someecardsLinksJson;
+    const calendar = createCalendar(calendarYear);
+    const calendarObject = {
+        birthdayUrl: '',
+        anniversaryUrl: '',
+        calendarUrls: []
+    };
 
-		if ( visualElements.includes(req.resourceType()) ) {
-			req.abort();
-        } else {
-            req.continue();
+    /* Collect specialty categories */
+    const weekendCategories = getSpecificCategoryUrls(categories, 'weekend');
+    const birthdayCategories = getSpecificCategoryUrls(categories, 'birthday');
+    const anniversaryCategories = getSpecificCategoryUrls(categories, 'anniversary');
+
+    delete categories['weekend'];
+    delete categories['birthday'];
+    delete categories['anniversary'];
+
+    calendarObject.birthdayUrl = getCategoryUrl(birthdayCategories);
+    calendarObject.anniversaryUrl = getCategoryUrl(anniversaryCategories);
+
+    /* Load config.json options */
+    const { specialHolidayEvents } = config;
+
+    for ( let specialHolidayEvent of specialHolidayEvents ) {
+        const { startDate, endDate, slug } = specialHolidayEvent;
+        const startDateString = `${calendarYear}-${startDate}`;
+        const endDateString = `${calendarYear}-${endDate}`;
+        const holidayMonth = new Date(startDateString).getMonth() + 1;
+        const holidayMonthCategoryUrls = categories && categories[slug] ? categories[slug] : [];
+
+        if ( holidayMonthCategoryUrls.length ) {
+            specialHolidayEvent.urls = holidayMonthCategoryUrls;
+            specialHolidayEvent.startDate = startDateString;
+            specialHolidayEvent.endDate = endDateString;
+
+            eventsByMonth[holidayMonth]['events'].push(specialHolidayEvent);
+            delete categories[slug];
         }
-	});
+    }
 
-	return Promise.resolve();
+    for ( let dateString of calendar ) {
+        const date = new Date(dateString);
+        const timestamp = date.setUTCHours(0,0,0,0); 
+        const month = date.getMonth() + 1;
+        const day = date.getDay();
+        const useWeekendCategory = weekendCategories.length && [5].includes(day); // 5 is Friday. 0 is Sunday. 6 is Saturday
+        const holidayUrls = getHolidayUrls(eventsByMonth[month], timestamp);
+        const categoryUrl = useWeekendCategory ? getCategoryUrl(weekendCategories) : getCategoryUrl(categories);
+
+        /* TODO: deal with Seasonal category */
+
+        // console.log({holidayUrls});
+        // console.log({categoryUrl});
+        // console.log({date, isWeekend});
+
+        calendarObject.calendarUrls.push({
+            date,
+            day,
+            holidayUrls,
+            categoryUrl
+        });
+    }
+
+    return Promise.resolve(calendarObject);
 }
 
 function createCalendar(year) {
@@ -69,6 +126,23 @@ function createCalendar(year) {
     }
 
     return calendar;
+}
+
+function getSpecificCategoryUrls( categories, categoryName ) {
+    const specificCategoryUrls = categories[categoryName] !== 'undefined'
+        ? categories[categoryName]
+        : [];
+    return {
+        categoryName: specificCategoryUrls
+    };
+}
+
+function getCategoryUrl(categories) {
+    const categoryTypes = Object.keys(categories);
+    const randomCategory = getRandomArrayItem(categoryTypes); 
+    const randomUrl = getRandomArrayItem(categories[randomCategory]);
+
+    return getUniqueUrl(randomUrl, getCategoryUrl, categories);
 }
 
 function getHolidayUrls(eventsByCurrentMonth, currentDate) {
@@ -115,97 +189,30 @@ function getUniqueUrl(url, callback, callbackParam) {
     return url;
 }
 
-function getCategoryUrl(categories) {
-    const categoryTypes = Object.keys(categories);
-    const randomCategory = getRandomArrayItem(categoryTypes); 
-    const randomUrl = getRandomArrayItem(categories[randomCategory]);
+async function ignoreVisualElements(page) {
+    await page.setRequestInterception(true);
+    page.on('request', (req) => {
+		const visualElements = ['image', 'stylesheet', 'font'];
 
-    return getUniqueUrl(randomUrl, getCategoryUrl, categories);
-}
+		if ( visualElements.includes(req.resourceType()) ) {
+			req.abort();
+        } else {
+            req.continue();
+        }
+	});
 
-function getSpecificCategoryUrls( categories, categoryName ) {
-    const specificCategoryUrls = categories[categoryName] !== 'undefined'
-        ? categories[categoryName]
-        : [];
-    return {
-        categoryName: specificCategoryUrls
-    };
+	return Promise.resolve();
 }
 
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    const someecardsLinks = await fetch(someecardsLinksUrl);
-    const someecardsLinksJson = Object.keys(localSomeecardsLinksJson).length
-        ? localSomeecardsLinksJson 
-        : await someecardsLinks.json(); 
-    const { baseUrl, categories, eventsByMonth } = someecardsLinksJson;
 
 	/* Improve script speed by not loading visual resources */
     await ignoreVisualElements(page);
 
     const calendarYear = 2020;
-    const calendar = createCalendar(calendarYear);
-    const calendarObject = {
-        birthdayUrl: '',
-        anniversaryUrl: '',
-        calendarUrls: []
-    };
-
-    /* Collect specialty categories */
-    const weekendCategories = getSpecificCategoryUrls(categories, 'weekend');
-    const birthdayCategories = getSpecificCategoryUrls(categories, 'birthday');
-    const anniversaryCategories = getSpecificCategoryUrls(categories, 'anniversary');
-
-    delete categories['weekend'];
-    delete categories['birthday'];
-    delete categories['anniversary'];
-
-    calendarObject.birthdayUrl = getCategoryUrl(birthdayCategories);
-    calendarObject.anniversaryUrl = getCategoryUrl(anniversaryCategories);
-
-    /* Load config.json options */
-    const { specialHolidayEvents } = config;
-
-    for ( let specialHolidayEvent of specialHolidayEvents ) {
-        const { startDate, endDate, slug } = specialHolidayEvent;
-        const startDateString = `${calendarYear}-${startDate}`;
-        const endDateString = `${calendarYear}-${endDate}`;
-        const holidayMonth = new Date(startDateString).getMonth() + 1;
-        const holidayMonthCategoryUrls = categories && categories[slug] ? categories[slug] : [];
-
-        if ( holidayMonthCategoryUrls.length ) {
-            specialHolidayEvent.urls = holidayMonthCategoryUrls;
-            specialHolidayEvent.startDate = startDateString;
-            specialHolidayEvent.endDate = endDateString;
-
-            eventsByMonth[holidayMonth]['events'].push(specialHolidayEvent);
-            delete categories[slug];
-        }
-    }
-
-    for ( let dateString of calendar ) {
-        const date = new Date(dateString);
-        const timestamp = date.setUTCHours(0,0,0,0); 
-        const month = date.getMonth() + 1;
-        const day = date.getDay();
-        const useWeekendCategory = weekendCategories.length && [5].includes(day); // 5 is Friday. 0 is Sunday. 7 is Saturday
-        const holidayUrls = getHolidayUrls(eventsByMonth[month], timestamp);
-        const categoryUrl = useWeekendCategory ? getCategoryUrl(weekendCategories) : getCategoryUrl(categories);
-
-        /* TODO: deal with Seasonal category */
-
-        // console.log({holidayUrls});
-        // console.log({categoryUrl});
-        // console.log({date, isWeekend});
-
-        calendarObject.calendarUrls.push({
-            date,
-            day,
-            holidayUrls,
-            categoryUrl
-        });
-    }
+    const calendarObject = await getCalendarObject(calendarYear)
 
     /* DEBUG: view full calendarObject */
     const util = require('util');
